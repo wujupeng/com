7ef4cc916d6ac6f155b2e30364c538a3a02d2b1c
wujupeng <hunter@asml.sh.cn>
2025-11-24 16:53:52 +0800

    Initial commit: Project setup and basic UI

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace systemcopyfile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new AppShell());
+	}
+}
